00ce72f79dea5c2bd7129655fc865d94e3ebaf40
Alex Cook <apcook7104@gmail.com>
2021-01-02 22:24:33 -0600

    Add more output variables

--- terraform-course/main.tf
+++ terraform-course/main.tf
@@ -104,3 +104,11 @@ resource "aws_eip" "web_server_public_ip" {
 output "ec2_public_ip" {
   value = aws_eip.web_server_public_ip.public_ip
 }
+
+output "ec2_private_ip" {
+  value = aws_instance.web_server.private_ip
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.web_server.id
+}
