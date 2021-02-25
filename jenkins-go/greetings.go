4674a4159ed0b08feb4e86dffe0921910dc2b639
Alex Cook <apcook7104@gmail.com>
2021-02-25 16:02:47 -0700

    Add greeting feed function and test

--- jenkins-go/greetings.go
+++ jenkins-go/greetings.go
@@ -1,5 +1,28 @@
 package greetings
 
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// HelloWorld returns the greeting "Hello, world!"
 func HelloWorld() string {
 	return "Hello, world!"
+}
+
+// GreetingFeed takes the slice of greetings input and sends
+// the greetings in a random order on the returned channel.
+func GreetingFeed(greetings []string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for _, v := range rand.Perm(len(greetings)) {
+			ch <- greetings[v]
+		}
+		close(ch)
+	}()
+	return ch
 }
--- jenkins-go/greetings_test.go
+++ jenkins-go/greetings_test.go
@@ -1,11 +1,55 @@
 package greetings
 
 import (
+	"fmt"
 	"testing"
 )
 
+// TestHelloWorld tests the HelloWorld function.
 func TestHelloWorld(t *testing.T) {
 	if result := HelloWorld(); result != "Hello, world!" {
 		t.Errorf("HelloWorld() = %s, want \"Hello, world!\"", result)
+	}
+}
+
+// Helper function for TestGreetingFeed.
+func findAndRemoveGreeting(s string, greetings []string) ([]string, error) {
+	for i, greeting := range greetings {
+		if s == greeting {
+			greetings[i] = greetings[len(greetings)-1]
+			return greetings[:len(greetings)-1], nil
+		}
+	}
+	return nil, fmt.Errorf("greeting %v not in %v", s, greetings)
+}
+
+// TestGreetingFeed tests the GreetingFeed function.
+func TestGreetingFeed(t *testing.T) {
+	testCases := []struct {
+		name string
+		in   []string
+	}{
+		{
+			name: "basic",
+			in:   []string{"Hello!", "Goodbye!", "Greetings!"},
+		},
+		{
+			name: "unicode",
+			in:   []string{"こんにちは", "Привет", "أهلا"},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			ch := GreetingFeed(test.in)
+			for greeting := range ch {
+				var err error
+				test.in, err = findAndRemoveGreeting(greeting,
+					test.in)
+				if err != nil {
+					t.Errorf(err.Error())
+				}
+			}
+		})
 	}
 }
