eabb836abfb6ae83c8b1501090976c088c89fb2a
Alex Cook <apcook7104@gmail.com>
2021-02-26 13:46:13 -0700

    Add AdministratorAccess policy to admin group

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -1,11 +1,14 @@
 ### TODO ###
-# 1. Add AdministratorAccess policy to aws_iam_group.
 # 2. Add IAMUserChangePassword policy to aws_iam_user.
 # 3. Create a role granting full S3 access to EC2 instances.
 # 4. Create CloudWatch billing alarm with SNS alert.
 
 provider "aws" {
   region = var.region
+}
+
+data "aws_iam_policy" "AdministratorAccess" {
+  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_user" "user" {
@@ -23,4 +26,9 @@ resource "aws_iam_user_group_membership" "membership" {
   groups = [
     aws_iam_group.group.name,
   ]
+}
+
+resource "aws_iam_group_policy_attachment" "group_policy" {
+  group      = aws_iam_group.group.name
+  policy_arn = data.aws_iam_policy.AdministratorAccess.arn
 }
