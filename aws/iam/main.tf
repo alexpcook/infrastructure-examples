cccfdc35376d0f10b9a0857c702414b0091b0c91
Alex Cook <apcook7104@gmail.com>
2021-02-26 12:46:27 -0700

    Remove module statements

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "iam" {
-  source  = "terraform-aws-modules/iam/aws"
-  version = "3.9.0"
-}
-
 resource "aws_iam_user" "user" {
   name = "user1"
   tags = {
