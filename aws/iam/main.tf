7cfb9dcd059ee77a7a48c2b2db20df985e20bd4b
Alex Cook <apcook7104@gmail.com>
2021-02-26 12:34:30 -0700

    Add iam users to iam groups

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -17,3 +17,13 @@ resource "aws_iam_user" "user" {
 resource "aws_iam_group" "group" {
   name = "my-users"
 }
+
+resource "aws_iam_group_membership" "team" {
+  name = "my-users-membership"
+
+  users = [
+    aws_iam_user.user.name,
+  ]
+
+  group = aws_iam_group.group.name
+}
