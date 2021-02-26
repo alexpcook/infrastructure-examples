fbb689c635fd55b8b1883b0ea42d80b45e4766bc
Alex Cook <apcook7104@gmail.com>
2021-02-26 12:08:56 -0700

    Add iam user

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -1,4 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "iam" {
   source  = "terraform-aws-modules/iam/aws"
   version = "3.9.0"
+}
+
+resource "aws_iam_user" "user" {
+  name = "user1"
+  tags = {
+    created-by-tf = "true"
+  }
 }
