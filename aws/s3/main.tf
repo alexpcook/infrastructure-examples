8aacd61a73b041939be63795be422b8f8339fe56
Alex Cook <apcook7104@gmail.com>
2021-02-28 20:58:29 -0800

    Restrict public access to buckets by default

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -25,3 +25,19 @@ resource "aws_s3_bucket_object" "private" {
   source   = join("/", [var.source_directory, each.value])
   etag     = filemd5(join("/", [var.source_directory, each.value]))
 }
+
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket                  = aws_s3_bucket.public.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
