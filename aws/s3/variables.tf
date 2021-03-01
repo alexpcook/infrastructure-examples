c6a6739a80d89210b24b2c4de0f0765baa2c2a73
Alex Cook <apcook7104@gmail.com>
2021-02-28 18:16:23 -0800

    Remove not needed variables

--- aws/s3/variables.tf
+++ aws/s3/variables.tf
@@ -2,28 +2,3 @@ variable "region" {
   type    = string
   default = "us-west-1"
 }
-
-variable "users" {
-  type    = list(string)
-  default = ["alex.cook"]
-}
-
-variable "groups" {
-  type    = list(string)
-  default = ["administrators"]
-}
-
-variable "path" {
-  type    = string
-  default = "/acg/"
-}
-
-variable "ec2_sts_assume_role_policy_json" {
-  type    = string
-  default = "ec2_sts_assume_role_policy.json"
-}
-
-variable "pgp_key" {
-  type    = string
-  default = "keybase:alexpcook"
-}
