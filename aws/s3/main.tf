586c27ac0738e0825982f0fffc7754aa5c9d1d83
Alex Cook <apcook7104@gmail.com>
2021-02-28 20:31:58 -0800

    Add s3 objects to private bucket

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -10,9 +10,17 @@ resource "aws_s3_bucket" "private" {
   bucket_prefix = join("-", [var.bucket_prefix, "private"])
 }
 
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_bucket_object" "public" {
   for_each = fileset(format("%s/", var.source_directory), "*")
   bucket   = aws_s3_bucket.public.id
+  key      = each.value
+  source   = join("/", [var.source_directory, each.value])
+  etag     = filemd5(join("/", [var.source_directory, each.value]))
+}
+
+resource "aws_s3_bucket_object" "private" {
+  for_each = fileset(format("%s/", var.source_directory), "*")
+  bucket   = aws_s3_bucket.private.id
   key      = each.value
   source   = join("/", [var.source_directory, each.value])
   etag     = filemd5(join("/", [var.source_directory, each.value]))
