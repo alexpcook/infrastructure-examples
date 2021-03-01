88c58d2e6cd1ec708094a727c0560e005c7264f4
Alex Cook <apcook7104@gmail.com>
2021-03-01 14:54:31 -0800

    Add basic cloudwatch alarm config

--- aws/cloudwatch/main.tf
+++ aws/cloudwatch/main.tf
@@ -1,3 +1,19 @@
 provider "aws" {
   region = var.region
 }
+
+resource "aws_cloudwatch_metric_alarm" "total_cost" {
+  alarm_name        = "${var.name_prefix}-costs-exceed-20-usd"
+  alarm_description = "Send message to SNS topic when costs exceed $20"
+
+  namespace           = "AWS/Billing"
+  metric_name         = "EstimatedCharges"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "20"
+
+  evaluation_periods = "1"
+  period             = "21600"
+  statistic          = "Maximum"
+
+  // alarm_actions = TODO
+}
--- aws/cloudwatch/variables.tf
+++ aws/cloudwatch/variables.tf
@@ -1,4 +1,9 @@
 variable "region" {
-  type = string
-  default = "us-west-1"
+  type    = string
+  default = "us-east-1"
+}
+
+variable "name_prefix" {
+  type    = string
+  default = "acg"
 }
