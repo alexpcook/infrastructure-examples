e6a1c371e65f481354a4640a330d2ec317e7f7e3
Alex Cook <apcook7104@gmail.com>
2021-02-28 19:06:22 -0800

    Add S3 objects to buckets

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -3,9 +3,21 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "b1" {
-  bucket_prefix = "acg"
+  bucket_prefix = var.bucket_prefix
 }
 
 resource "aws_s3_bucket" "b2" {
-  bucket_prefix = "acg"
+  bucket_prefix = var.bucket_prefix
+}
+
+resource "aws_s3_bucket_object" "o1" {
+  bucket = aws_s3_bucket.b1.id
+  key    = "version"
+  source = "src/version.txt"
+}
+
+resource "aws_s3_bucket_object" "o2" {
+  bucket = aws_s3_bucket.b2.id
+  key    = "animals"
+  source = "src/animals.html"
 }
--- aws/s3/variables.tf
+++ aws/s3/variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   type    = string
   default = "us-west-1"
 }
+
+variable "bucket_prefix" {
+  type    = string
+  default = "acg-"
+}
