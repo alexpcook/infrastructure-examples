704b27f5af5c1c6ce1c726fd99c0bc9db1026e8c
Alex Cook <apcook7104@gmail.com>
2021-03-01 09:49:57 -0800

    Add s3 bucket server-side encryption

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -4,10 +4,24 @@ provider "aws" {
 
 resource "aws_s3_bucket" "public" {
   bucket_prefix = join("-", [var.bucket_prefix, "public"])
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket" "private" {
   bucket_prefix = join("-", [var.bucket_prefix, "private"])
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_object" "public" {
