f823b8a32bcc3c5c8085bb68f1a50ea50b5504f8
Alex Cook <apcook7104@gmail.com>
2021-03-09 17:52:38 -0800

    Fix subnet numbering issue

--- aws/vpc/variables.tf
+++ aws/vpc/variables.tf
@@ -1,7 +1,7 @@
 variable "profile" {
   description = "The AWS profile to deploy to"
   type        = string
-  # This can be set with an environment variable using TF_VAR_* syntax
+  # This can be set with an environment variable called TF_VAR_profile
 }
 
 variable "region" {
@@ -14,9 +14,9 @@ variable "vpcs" {
   description = "The networks to deploy to AWS"
   type        = map(string)
   default = {
-    "net1" : "10.0.0.0/24"
-    "net2" : "10.1.0.0/24"
-    "net3" : "10.2.0.0/24"
+    "net1" : "10.1.0.0/24"
+    "net2" : "10.2.0.0/24"
+    "net3" : "10.3.0.0/24"
   }
 }
 
@@ -24,8 +24,8 @@ variable "subnets" {
   description = "The subnets to deploy to each AWS VPC"
   type        = map(list(string))
   default = {
-    "net1" : ["10.0.0.0/28", "10.0.0.16/28"],
-    "net2" : ["10.0.0.0/28", "10.0.0.16/28"],
-    "net3" : ["10.0.0.0/28", "10.0.0.16/28"],
+    "net1" : ["10.1.0.0/28", "10.1.0.16/28"],
+    "net2" : ["10.2.0.0/28", "10.2.0.16/28"],
+    "net3" : ["10.3.0.0/28", "10.3.0.16/28"],
   }
 }
