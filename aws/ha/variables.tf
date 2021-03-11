9a46707ffadff39a6145e591ea0d103fe5311df7
Alex Cook <apcook7104@gmail.com>
2021-03-11 12:02:33 -0800

    Add naming variables

--- aws/ha/variables.tf
+++ aws/ha/variables.tf
@@ -3,3 +3,15 @@ variable "profile" {
   type        = string
   # Set using TF_VAR_profile
 }
+
+variable "dl" {
+  description = "The delimiter to use in AWS resource names/tags"
+  type        = string
+  default     = "-"
+}
+
+variable "name_prefix" {
+  description = "The name prefix to append to AWS resources"
+  type        = string
+  default     = "apc-acg"
+}
