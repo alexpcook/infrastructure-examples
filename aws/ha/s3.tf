2105c407bfc5c9b1c0239fd4d456026b0e946b2a
Alex Cook <apcook7104@gmail.com>
2021-03-12 11:08:58 -0800

    Add public read bucket policy to media bucket

--- aws/ha/s3.tf
+++ aws/ha/s3.tf
@@ -10,8 +10,29 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = each.value.id
 
   // allow public access only to media bucket
-  block_public_acls       = each.key == var.s3_bucket_names[1] ? false : true
-  block_public_policy     = each.key == var.s3_bucket_names[1] ? false : true
-  ignore_public_acls      = each.key == var.s3_bucket_names[1] ? false : true
-  restrict_public_buckets = each.key == var.s3_bucket_names[1] ? false : true
+  block_public_acls       = each.key == local.media_bucket_key ? false : true
+  block_public_policy     = each.key == local.media_bucket_key ? false : true
+  ignore_public_acls      = each.key == local.media_bucket_key ? false : true
+  restrict_public_buckets = each.key == local.media_bucket_key ? false : true
+}
+
+resource "aws_s3_bucket_policy" "allow_public_reads" {
+  bucket = aws_s3_bucket.wp[local.media_bucket_key].id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.wp[local.media_bucket_key].arn}/*"
+        ]
+      }
+    ]
+  })
 }
