45c607ccc58aa9da42150fc48b6ca3e67c9a64f4
Alex Cook <apcook7104@gmail.com>
2021-03-11 16:55:25 -0800

    Add iam role for ec2 access to s3

---
+++ aws/ha/iam.tf
@@ -0,0 +1,12 @@
+data "aws_iam_policy" "AmazonS3FullAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role" "ec2_s3_full_access" {
+  name               = "ec2_full_access_to_s3"
+  description        = "Grant EC2 instances full access to S3."
+  assume_role_policy = file("ec2_sts_assume_role_policy.json")
+  managed_policy_arns = [
+    data.aws_iam_policy.AmazonS3FullAccess.arn,
+  ]
+}
